38cf4cccdc7f90d84b1c3effc242d8aed48629ba
kamalweheliye <kamalweheliye@gmail.com>
2025-11-01 13:27:50 +0400

    call http concurrently

---
+++ go-23-csp/go_23_csp_01.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type result struct {
+	url     string
+	err     error
+	latency time.Duration
+}
+
+func get(url string, ch chan<- result) {
+
+	start := time.Now()
+	if resp, err := http.Get(url); err != nil {
+		ch <- result{}
+	} else {
+		t := time.Since(start).Round(time.Millisecond)
+		ch <- result{url, nil, t}
+		resp.Body.Close()
+	}
+}
+
+func main() {
+
+	result := make(chan result)
+	urls := []string{
+		"https://www.amazon.com",
+		"https://www.google.com",
+		"https://www.nytimes.com",
+		"https://www.wsj.com",
+	}
+
+	start := time.Now()
+	for _, url := range urls {
+		go get(url, result)
+	}
+
+	for range urls {
+		r := <-result
+		if r.err != nil {
+			log.Printf("%s failed: %v", r.url, r.err)
+		} else {
+			log.Printf("%s took %s", r.url, r.latency)
+		}
+
+	}
+
+	log.Printf("%d urls took %s", len(urls), time.Since(start))
+
+}
